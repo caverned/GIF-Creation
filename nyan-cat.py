8444c46c926879521216e41d0298b1697c958503
Carl Llido <s4007394@student.rmit.edu.au>
2025-11-24 14:54:41 +1100

    Moved to appropiate folders

--- nyan-cat.py
+++
@@ -1,9 +0,0 @@
-import imageio.v3 as iio
-
-filenames = ['nyan-cat1.png', 'nyan-cat2.png', 'nyan-cat3.png']
-images = []
-
-for filename in filenames:
-    images.append(iio.imread(filename))
-
-iio.imwrite('nyan-cat.gif', images, duration=250, loop=0)
